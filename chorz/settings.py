f9bbc1a8c9ab462fb2a52ef865865109f455ee91
Theodore Hu <theodore.n.hu@gmail.com>
2018-08-12 09:02:06 -0700

    Fix Error 400

--- chorz/settings.py
+++ chorz/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dx+8-615@z%sfgia=%dv7*1(d(^aa$
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['chorz-manager.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['chorz-manager.herokuapp.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
