d92953fbc9dbdf5724fedaad3ad3955234eaa56a
Theodore Hu <theodore.n.hu@gmail.com>
2018-08-12 10:16:03 -0700

    Add whitenoise

--- chorz/settings.py
+++ chorz/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'),)
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
